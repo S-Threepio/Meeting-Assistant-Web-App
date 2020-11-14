3485b881c8f69735d1daa7297b7486fe8faf7170
cult_mentality <kavitkarswanand5@gmail.com>
2020-11-14 22:30:59 +0530

    dynamo db integrated

--- src/components/Tables/TableComponent.js
+++ src/components/Tables/TableComponent.js
@@ -1,30 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Sprints.css'
 import sprints from '../../data/sprints.json'
 import { Row, Col } from 'react-simple-flex-grid';
 import "react-simple-flex-grid/lib/main.css";
+import axios from '../../data/axios';
+import AWS from 'aws-sdk';
 
 
-const TableComponent = () => { 
+const TableComponent = () => {
+  const [results, setResults] = useState([]);
 
-  function handleClick(props){
-    alert("sprint "+props+" is clicked")
+  function handleClick(props) {
+    alert("sprint " + props + " is clicked")
+    console.log(props)
   }
 
+  function showTables(props) {
+    console.log(props)
+    return (
+      <div className="content-table">
+        <Row gutter={40}>
+          {props.map(co =>
+            <Col gutter={50}
+              xs={{ span: 6 }} sm={{ span: 5 }} md={{ span: 5 }}
+              lg={{ span: 5 }} xl={{ span: 4 }}
+            ><div className="sprint" onClick={() => handleClick(co.id)}><div id="head">{co.id}</div><div id="body" >{co.mom}</div></div></Col>
+          )}
+        </Row>
 
-  return (
-      <div class="content-table">
-<Row gutter={40}>
-  {sprints.map(co => 
-    <Col gutter={50}
-      xs={{ span: 6 }} sm={{ span: 5 }} md={{ span: 5 }}
-      lg={{ span: 5 }} xl={{ span: 4 }}
-     ><div class="sprint" onClick={()=>handleClick(co.name)}><div id="head">{co.name}</div><div id="body" >{co.data}</div></div></Col>
-  )}
-</Row>
-      
       </div>
     );
   }
-  
+
+  // fetch(sign_in, {
+  //     mode: 'cors',
+  //     credentials: 'include',
+  //     method: 'GET',
+  //     headers: headers
+  // })
+
+
+  useEffect(() => {
+    axios
+      .get(`/items`)
+      .then(res => {
+        res.data.Items.forEach(element => {
+          setResults(prevResults => ([...prevResults, AWS.DynamoDB.Converter.unmarshall(element)]))  
+        });
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+
+  return (results ? showTables(results) : null)
+
+}
+
 export default TableComponent
